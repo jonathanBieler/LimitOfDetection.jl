59975fa1228f9fce0db056940ff9f75ed8290d96
Documenter.jl <documenter@juliadocs.github.io>
2023-05-31 14:34:54 +0000

    build based on 7344beb

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = LimitOfDetection","category":"page"},{"location":"#LimitOfDetection","page":"Home","title":"LimitOfDetection","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LimitOfDetection.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [LimitOfDetection]","category":"page"}]
+}
